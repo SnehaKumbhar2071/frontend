c7ebd00d03132c588f5c760c35ed00f4afb27ee8
Gautami7448 <khedkargautami@gmail.com>
2025-04-03 19:19:31 +0530

    BookTrackingPage is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Booklist from "./Pages/Booklist";
+import BookTrackingPage from "./Pages/BookTrackingPage";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Booklist />
+      <BookTrackingPage />
     </>
   );
 }
